b305886dad7c2947c2c266f023750541b807c189
Adib Saikali <asaikali@gmail.com>
2020-11-24 20:24:49 -0500

    Add Greeting Client

---
+++ greeting-client/src/test/java/com/example/mtls/client/GreetingClientApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.mtls.client;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GreetingClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
