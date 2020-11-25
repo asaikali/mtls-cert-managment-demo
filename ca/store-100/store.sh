9c108f0e2ded73b9b79c9af5ae6605c18a62d557
Adib Saikali <asaikali@gmail.com>
2020-11-25 01:04:16 -0500

    Add two store certs

---
+++ ca/store-100/store.sh
@@ -0,0 +1,11 @@
+
+# Generate the server certificate
+openssl req -nodes  -newkey rsa:2048 -keyout store.key -out store.csr \
+ -subj "/C=CA/ST=Ontario/L=Toronto/O=ACME Inc/OU=pharmacy/CN=store-100"
+
+# Sign the server certificate
+openssl x509 -req -CA ../root/rootCA.crt -CAkey ../root/rootCA.key -in store.csr -out store.crt -days 365 -CAcreateserial -extfile localhost.ext
+
+# generate the jks file for the server
+openssl pkcs12 -export -out store.p12 -name "store" -inkey store.key -in store.crt
+#keytool -importkeystore -srckeystore server.p12 -srcstoretype PKCS12 -destkeystore server.jks -deststoretype JKS -storepass password
